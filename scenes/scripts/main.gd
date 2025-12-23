635578b4274ab8bc4a333bc20cebfde0f6cd9837
EggyWorking <37382711+AwesomeYT2005@users.noreply.github.com>
2025-12-23 04:39:12 +0000

    Basic objects
    Implemented a door, a fully moveable and interactable box, a functional button, and... a way out

--- scenes/scripts/main.gd
+++ scenes/scripts/main.gd
@@ -2,6 +2,8 @@ extends Control
 
 @onready var player: CharacterBody2D = $Player
 @onready var box: RigidBody2D = $Box
+@onready var door: AnimatableBody2D = $ExitDoor
+var exit_door_state: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,5 +11,17 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics(delta: float) -> void:
-	pass
+func _process(delta: float) -> void:
+	if exit_door_state:
+		door.position = Vector2(1300,563)
+	else:
+		door.position = Vector2(1136,563)
+
+
+func _on_button_body_entered(body: Node2D) -> void:
+	exit_door_state = true
+
+
+
+func _on_button_body_exited(body: Node2D) -> void:
+	exit_door_state = false
