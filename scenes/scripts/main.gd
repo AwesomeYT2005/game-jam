b583ccac3c94177adef8146ea2803d53bc22f0c1
EggyWorking <37382711+AwesomeYT2005@users.noreply.github.com>
2025-12-23 03:37:37 +0000

    Added box object
    Box object, can be pushed by the player, currently lock rotation is off but that can be changed in the future.

---
+++ scenes/scripts/main.gd
@@ -0,0 +1,13 @@
+extends Control
+
+@onready var player: CharacterBody2D = $Player
+@onready var box: RigidBody2D = $Box
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics(delta: float) -> void:
+	pass
--- scenes/scripts/player.gd
+++ scenes/scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const SPEED = 400.0
 const JUMP_VELOCITY = -600.0
+const PUSH_FORCE = 100.0
 
 
 func _physics_process(delta: float) -> void:
@@ -20,5 +21,12 @@ func _physics_process(delta: float) -> void:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	# Pushing blocks.
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var collision_box = collision.get_collider()
+		if collision_box is RigidBody2D:
+			collision_box.apply_central_impulse(collision.get_normal() * -PUSH_FORCE)
 
 	move_and_slide()
